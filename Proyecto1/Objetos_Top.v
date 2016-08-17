14169cf384117d5c642c1d296bf5d114e76b96c3
Santy241996 <santiago241996@gmail.com>
2016-08-17 17:55:49 -0600

    modificación, paso de binario a hexadecimal en el control de dirección (me equivoque y subí el que no era)

--- Proyecto1/Objetos_Top.v
+++ Proyecto1/Objetos_Top.v
@@ -17,9 +17,9 @@ output reg [2:0] L;     //Despues de ser condicionadas estas senales son las que
 
 ///////////////////   Cables de seccion de Almacenamiento de objetos  /////////////////// 
 // Entradas 
-reg [3:0]PY; 
-reg [2:0]PX; 
-reg [2:0]EN; 
+wire [3:0]PY = {Y[3],Y[2],Y[1],Y[0]};
+wire [2:0]PX = {X[2],X[1],X[0]};
+reg  [3:0]EN; 
 
 
 ///////////////////          Cables de seccion de filtro              /////////////////// 
@@ -29,67 +29,84 @@ reg Activador;  //A grandes razgos es el que determina si las senales R,G,B lleg
 
 ///////////////////     Submodulo "Selector de Objetos"     /////////////////// 
 
-wire [6:0] SelectorX1 = {X[9],X[8],X[7],X[6],X[5],X[4],X[3]};
-wire [2:0] SelectorX2 = {X[2],X[1],X[0]};
+localparam X_in_J  = 0;
+localparam X_end_J = 7;
 
-wire [5:0] SelectorY1 = {Y[9],Y[8],Y[7],Y[6],Y[5],Y[4]};
-wire [3:0] SelectorY2 = {Y[3],Y[2],Y[1],Y[0]};
+localparam X_in_V  = 8;
+localparam X_end_V = 15;
+
+localparam X_in_M  = 23;
+localparam X_end_M = 30;
+
+localparam X_in_B  = 31;
+localparam X_end_B = 38;
+ 
+localparam X_in_S  = 46;
+localparam X_end_S = 53;
+
+localparam X_in_L  = 54;
+localparam X_end_L = 60;
 
 
-always @(X,Y)                 //A partir de las entradas X y Y se determinan PX, PY y EN, que son las senales responsables de encontrar el dato deseado 
-begin 
-	case (SelectorX1)                     //Se determina EN
-	7'b0000000 : EN = 3'b000;
-	7'b0001000 : EN = 3'b001;
-	7'b0001111 : EN = 3'b010;
-	7'b0011000 : EN = 3'b011;
-	7'b0100000 : EN = 3'b100;
-	7'b0101000 : EN = 3'b101;
-	default : EN = 3'b111;
-	endcase 
+localparam Y_in  = 0;
+localparam Y_end = 15;
 
-	case (SelectorX1)                     //Se determina PX	
-	7'b0000000 : PX = SelectorX2;
-	7'b0001000 : PX = SelectorX2;
-	7'b0001111 : PX = SelectorX2;
-	7'b0011000 : PX = SelectorX2;
-	7'b0100000 : PX = SelectorX2;
-	7'b0101000 : PX = SelectorX2;
-	default : PX = 3'b000;
-	endcase 
+wire J_on,V_on,M_on,B_on,S_on,L_on;
 
-	case (SelectorY1)                     //Se determina PY	
-	6'b000000 : PY = SelectorY2;
-	default : PY = 4'b0000;	
-	endcase 
+assign J_on = (X_in_J <=X) && (X <=X_end_J) && (Y_in <=Y) && (Y <=Y_end);
+assign V_on = (X_in_V <=X) && (X <=X_end_V) && (Y_in <=Y) && (Y <=Y_end);
+assign M_on = (X_in_M <=X) && (X <=X_end_M) && (Y_in <=Y) && (Y <=Y_end);
+assign B_on = (X_in_B <=X) && (X <=X_end_B) && (Y_in <=Y) && (Y <=Y_end);
+assign S_on = (X_in_S <=X) && (X <=X_end_S) && (Y_in <=Y) && (Y <=Y_end);
+assign L_on = (X_in_L <=X) && (X <=X_end_L) && (Y_in <=Y) && (Y <=Y_end);
+
+
+always @(*)                 //A partir de las entradas X y Y se determinan PX, PY y EN, que son las senales responsables de encontrar el dato deseado 
+begin
+		if (J_on)
+			EN = 4'h1;
+		else 
+			if (V_on)
+			EN = 4'h2;
+		
+		else 
+			if (M_on)
+			EN = 4'h3;
+		
+		else
+			if (B_on)
+			EN = 4'h4;
+		
+		else 
+			if (S_on)
+			EN = 4'h5;
+		
+		else 
+			if (L_on)
+			EN = 4'h6;
+			
+		else 
+			EN = 4'h0;
 end 
 
 ///////////////////  Submodulo "Almacenamiento de objetos" /////////////////// 
 wire [7:0] Data;               //Esta es la salida de datos de la memoria ROM
 Almacenamiento ROM (EN,PY,Data);
 
-always @(PX)                   //El dato de la ROM posee 8 bits, pero solo se necesita 1, en esta seccion se escoge cual; dependiendo directamente del valor de PX 
+always @(*)                   //El dato de la ROM posee 8 bits, pero solo se necesita 1, en esta seccion se escoge cual; dependiendo directamente del valor de PX 
 begin 
 	case (PX)
-
-	4'b0000 : Activador = Data[0];
-	4'b0001 : Activador = Data[1];
-	4'b0010 : Activador = Data[2];
-	4'b0011 : Activador = Data[3];
-	4'b0100 : Activador = Data[4];
-	4'b0101 : Activador = Data[5];
-	4'b0110 : Activador = Data[6];
-	4'b0111 : Activador = Data[7];
-	4'b1000 : Activador = Data[8];
-	4'b1001 : Activador = Data[9];
-	4'b1010 : Activador = Data[10];
-	4'b1011 : Activador = Data[11];
-	4'b1100 : Activador = Data[12];
-	4'b1101 : Activador = Data[13];
-	4'b1110 : Activador = Data[14];
-	4'b1111 : Activador = Data[15];
 	
-	default : Activador = Data[0];
+	4'h0 : Activador <= Data[0];
+	4'h1 : Activador <= Data[1];
+	4'h2 : Activador <= Data[2];
+	4'h3 : Activador <= Data[3];
+	4'h4 : Activador <= Data[4];
+	4'h5 : Activador <= Data[5];
+	4'h6 : Activador <= Data[6];
+	4'h7 : Activador <= Data[7];
+	
+	default : Activador <= Data[0];
 	endcase 
 end  
 
