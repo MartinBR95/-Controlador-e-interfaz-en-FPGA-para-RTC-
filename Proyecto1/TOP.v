f646543c9903f023ad8b96d3f1ea772679a4d18a
Santy241996 <santiago241996@gmail.com>
2016-08-18 07:06:06 -0600

    TOP

---
+++ Proyecto1/TOP.v
@@ -0,0 +1,13 @@
+module TOP(R,G,B,L,clk,reset,hsinc,vsinc,ENclock);
+
+input wire R,G,B,reset,clk;
+output wire [2:0]L;
+output wire hsinc,vsinc,ENclock;
+
+wire [9:0]X,Y;
+
+vgasync Sincronizacion (clk,reset,hsinc,vsinc,ENclock,X,Y);
+
+Objetos objets (L,X,Y,R,G,B);
+
+endmodule 
