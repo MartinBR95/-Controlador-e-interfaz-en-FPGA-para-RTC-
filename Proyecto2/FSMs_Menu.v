f5e2e0440b1d42d9b54a7d6a34f8fb51f501b516
Martin <martinbarquero@hotmail.com>
2016-09-20 16:51:34 -0600

    Se modifica FSMs_Menu para diferente comportamiento

--- Proyecto2/FSMs_Menu.v
+++ Proyecto2/FSMs_Menu.v
@@ -6,6 +6,7 @@
 // Create Date:    15:51:55 09/13/2016 
 // Design Name: 
 // Module Name:    FSMs_Menu 
+// Module Name:    FSMs_Menu 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,12 +19,12 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module FSMs_Menu (IRQ,Barriba,Babajo,Bderecha,Bizquierda,Bcentro,RST,FRW,Acceso,Mod,Alarma,STW,CLK,DIR,Numup,Numdown,Punt);
+module FSMs_Menu (IRQ,Barriba,Babajo,Bderecha,Bizquierda,Bcentro,RST,FRW,Acceso,Mod,Alarma,STW,CLK,Dir,Numup,Numdown,Punt);
 
 input wire CLK,IRQ,Barriba,Babajo,Bderecha,Bizquierda,Bcentro,RST,FRW; //IRQ: interrupcion del RTC para temporizador,FRW:finalizo lectura/escritura
-output reg [2:0] DIR; //Direccion de memoria del rtc al que se apunta
+output reg [6:0] Dir; //Direccion de memoria del rtc al que se apunta
 output reg Acceso,Mod,Alarma,STW,Numup,Numdown; //Acceso: a control RTC, Mod: modificacion del RTC, Alarma:Apagar alarma,Num++/Num--:aumentar/disminuir valor contenido en la direccion actual
-output reg [2:0] Punt;//Es un puntero que guarda la direccion donde se estan editando los valores
+output reg [6:0] Punt;//Es un puntero que guarda la direccion donde se estan editando los valores
 //////////////////////////////////Maquina de Estados Principal///////////////////////////////////////////////////
 localparam [7:0]TiempoEspera=5;
 //Registros de estado
@@ -39,17 +40,17 @@ reg Espera; //Indica a la maquina de estado que debe realizar un ciclo de espera
 reg [7:0] cuenta_espera;
 reg Fespera;//La maquina de estado de espera indica que termino la espera
 wire Fcount;
-reg [2:0] Punt_Siguiente;
+reg [6:0] Punt_Siguiente;
 wire cond1;
 assign cond1 = Barriba|Babajo|Bderecha|Bizquierda|Bcentro;
-assign Fcount=DIR==3'b111;
+assign Fcount=Dir==7'h44;
 //Valores Iniciales y asignacion de estado
 always @( posedge CLK,posedge RST)
 begin
 	if (RST)
 	begin
 		EstadoActual <= 3'd1 ; //Estado inicial
-		Punt<=3'd1;
+		Punt<=7'h21;
 		Mod<=1'b0;
 		Numup<=1'b0;
 		Numdown<=1'b0;
@@ -59,8 +60,6 @@ begin
 		EstadoActual <= EstadoSiguiente ;
 		Punt<=Punt_Siguiente;
 		Mod<=Mod_Siguiente;
-		Numup<=Numup_Siguiente;
-		Numdown<=Numdown_Siguiente;
 	end
 end
 //Logica Combinacional de siguiente estado y logica de salida
@@ -70,8 +69,6 @@ begin
 	Espera=1'b0;
 	Barrido=1'b0;
 	Mod_Siguiente=Mod;
-	Numup_Siguiente=Numup;
-	Numdown_Siguiente=Numdown;
 	Punt_Siguiente=Punt;
 	case(EstadoActual)
 	3'd1:if(FRW)
@@ -88,6 +85,7 @@ begin
 			Espera=1'b1;
 			EstadoSiguiente=3'd3;
 			Acceso=1'b0;
+			Mod_Siguiente=1'b0;
 		end
 		else
 		begin
@@ -104,32 +102,18 @@ begin
 		begin
 			EstadoSiguiente=3'd3;
 		end
-	3'd4:if(cond1)
+	3'd4:if(Bcentro)
 		begin		
-			Numup_Siguiente = Barriba;
-			Numdown_Siguiente = Babajo;
 			Mod_Siguiente=1'b1;
 			Barrido=1'b1;
-			if(Bcentro==1'b1)
-			begin
-				Punt_Siguiente = 3'd1;
-			end
-			else
-			begin
-				Punt_Siguiente = DIR + Bizquierda - Bderecha;
-			end
-			EstadoSiguiente = 3'd2;			
+			Punt_Siguiente=7'h21;
+			EstadoSiguiente = 3'd2;
 		end
 		else
 		begin
-			Acceso=1'b1;
-			EstadoSiguiente=3'd2;
 			Barrido=1'b1;
-			Mod_Siguiente=1'b0;
-			Numup_Siguiente=1'b0;
-			Numdown_Siguiente=1'b0;
-		end
-		
+			EstadoSiguiente = 3'd2;
+		end		
 	default
 	begin
 		EstadoSiguiente = 3'd1;
@@ -139,21 +123,21 @@ end
 
 //////////////////////////////////////Maquina de estados de Cuenta////////////////////////////////////
 //Registros de estado
-reg [1:0] EstadoActualc;
-reg [1:0] EstadoSiguientec;
-reg [2:0] DIRSiguiente;
+reg [2:0] EstadoActualc;
+reg [2:0] EstadoSiguientec;
+reg [6:0] Dir_Siguiente;
 //Valores Iniciales y asignacion de estado
 always@ ( posedge CLK, posedge RST )
 begin
 	if (RST)
 	begin
-		EstadoActualc <= 2'd1;
-		DIR <= 3'b1;
+		EstadoActualc <= 3'd1;
+		Dir <= 7'h02;
 	end
 	else
 	begin
 		EstadoActualc <= EstadoSiguientec;
-		DIR <= DIRSiguiente;
+		Dir <= Dir_Siguiente;
 	end
 end
 
@@ -161,39 +145,50 @@ end
 always @(*)
 begin
 	FBarrido=1'b0;
-	EstadoSiguientec = 2'd1;
-	DIRSiguiente = DIR;
+	EstadoSiguientec = 3'd1;
+	Dir_Siguiente = Dir;
 	case(EstadoActualc)	
-	2'd1:if(Barrido)
+	3'd1:if(Barrido)
 		begin
-			EstadoSiguientec=2'd2;		
+			EstadoSiguientec=3'd2;		
 		end
 		else
 		begin
-			EstadoSiguientec=2'd1;
+			EstadoSiguientec=3'd1;
+			Dir_Siguiente=7'h21;
 		end
-	2'd2:if(FRW)
+	3'd2:if(FRW)
 		begin	
-			DIRSiguiente = DIR + 1'b1;
-			EstadoSiguientec=2'd3;			
+			Dir_Siguiente = Dir + 1'b1;
+			EstadoSiguientec=3'd3;			
 		end
 		else
 		begin
-			EstadoSiguientec=2'd2;
+			EstadoSiguientec=3'd2;
 		end
-	2'd3:if(Fcount)
+	
+	3'd3:if(Dir==7'h27)
+		begin
+			Dir_Siguiente=7'h41;
+			EstadoSiguientec=3'd4;
+		end
+		else
+		begin
+			EstadoSiguientec=3'd4;
+		end
+	3'd4:if(Fcount)
 		begin
 			FBarrido=1'b1;
-			EstadoSiguientec=2'd1;
-			DIRSiguiente=1'b1;
+			EstadoSiguientec=3'd1;
+			Dir_Siguiente=7'h21;
 		end
 		else
 		begin	
-			EstadoSiguientec=2'd1;
+			EstadoSiguientec=3'd2;
 		end
 	default
 	begin
-		EstadoSiguientec = 2'd1;
+		EstadoSiguientec = 3'd1;
 	end	
 	endcase	
 end
