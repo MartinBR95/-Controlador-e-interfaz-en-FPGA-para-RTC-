74b9614baa4029e8ef1e12ee28a8e3168ee93363
MartinBR95 <martinbarquero@hotmail.com>
2016-09-28 17:49:37 -0600

    Actualizaciones a FSMs_Menu

--- Proyecto2/FSMs_Menu.v
+++ Proyecto2/FSMs_Menu.v
@@ -19,12 +19,12 @@
 // Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
-module FSMs_Menu (IRQ,Alarma_stop,Bderecha,Bizquierda,Bcentro,RST,FRW,Acceso,Mod,STW,CLK,Dir,Numup,Numdown,Punt);
+module FSMs_Menu (IRQ,Alarma_stop,Bderecha,Bizquierda,Bcentro,RST,FRW,Acceso,Mod,STW,CLK,Dir,Punt);
 
 input wire CLK,Alarma_stop,IRQ,Bderecha,Bizquierda,Bcentro,RST,FRW; //IRQ: interrupcion del RTC para temporizador,FRW:finalizo lectura/escritura
 output reg [7:0] Dir; //Direccion de memoria del rtc al que se apunta
 //output reg CMD; //Indicador de que se debe habilitar la dirección de comando F0 para transferir los datos de la RAM al RTC
-output reg Acceso,Mod,STW,Numup,Numdown; //Acceso: a control RTC, Mod: modificacion del RTC, Alarma:Apagar alarma,Num++/Num--:aumentar/disminuir valor contenido en la direccion actual
+output reg Acceso,Mod,STW; //Acceso: a control RTC, Mod: modificacion del RTC, Alarma:Apagar alarma,Num++/Num--:aumentar/disminuir valor contenido en la direccion actual
 output reg [6:0] Punt;//Es un puntero que guarda la direccion donde se estan editando los valores
 //////////////////////////////////Maquina de Estados Principal///////////////////////////////////////////////////
 localparam [7:0]TiempoEspera=8'd3;
@@ -45,6 +45,7 @@ reg Fespera;//La maquina de estado de espera indica que termino la espera
 wire Fcount;//Variable que indica el fin de la cuenta de direcciones
 reg [6:0] Punt_Siguiente;//variable a asignar a puntero en el ciclo de relog siguiente
 assign Fcount=Dir==8'hf1;//Constante de ultima direccion
+reg Bcentro_reg_ant;
 //Valores Iniciales y asignacion de estado
 //////////////////////////////////Maquina de estados Principal//////////////////////
 always @( posedge CLK,posedge RST)
@@ -53,32 +54,39 @@ begin
 	begin
 		EstadoActual <= 3'd1 ; //Estado inicial
 		Mod<=1'b1;
-		Numup<=1'b0;
-		Numdown<=1'b0;
 		Bcentro_reg<=1'b0;
+		Bcentro_reg_ant<=1'b0;
 		STW<=1'b0;
 	end
 	else
 	begin
 		EstadoActual <= EstadoSiguiente ;
 		Mod<=Mod_Siguiente;	
-		STW<=IRQ&&Alarma_stop;
-		if(Dir==8'h0)
+		STW<=IRQ&&Alarma_stop;		
+		
+		Bcentro_reg_ant<=Bcentro;			
+		if(~Bcentro_reg_ant && Bcentro)
 		begin
-			Bcentro_reg<=1'b0;
+			Bcentro_reg<=1'b1;
 		end
 		else
 		begin
-			Bcentro_reg<=Bcentro;
-		end
+			if(EstadoActual==3'd4)
+			begin
+				Bcentro_reg<=1'b0;	
+			end
+			else
+			begin
+				Bcentro_reg<=Bcentro_reg;
+			end
+		end		
 	end
 end
 
 //Logica Combinacional de siguiente estado y logica de salida
 always @(*)
 begin
-	if(Mod_Siguiente && Fespera) Mod_Siguiente = ~Mod_Siguiente;
-	else Mod_Siguiente = Mod;
+	Mod_Siguiente=Mod;
 	Espera=1'b0;
 	Barrido=1'b0;
 	if(FBarrido)AccesoCMD = 1'b1;
@@ -97,6 +105,7 @@ begin
 		begin
 			Espera=1'b1;//en caso de terminar el barrido de memoria se inicia la maquina de estados de espera
 			EstadoSiguiente=3'd3;
+			Mod_Siguiente=1'b0;
 		end
 		else
 		begin
@@ -232,23 +241,7 @@ begin
 				EstadoSiguientec=3'd4;
 			end
 		endcase
-			
-	
-	
-	
-	
-	
-	
-	
-	/*if(Dir==7'h27)
-		begin
-			Dir_Siguiente=7'h41;
-			EstadoSiguientec=3'd4;
-		end
-		else
-		begin
-			EstadoSiguientec=3'd4;
-		end*/
+
 	3'd4:if(Fcount)
 		begin
 			FBarrido=1'b1;
@@ -322,21 +315,47 @@ end
 
 
 /////////////////////////////////////Maquina de puntero///////////////////////////
+reg Bderecha_reg;
+reg Bderecha_reg_ant;//Valor anterior se usa para detectar el flanco
+reg Bizquierda_reg;
+reg Bizquierda_reg_ant;//Valor anterior se usa para detectar el flanco
 always@ ( posedge CLK, posedge RST )
 begin
 	if (RST)
 	begin
-		Punt<=7'h21;
+		Punt<=7'h21;		
+		Bderecha_reg<= 1'b0;
+		Bderecha_reg_ant<=1'b0;
+		Bizquierda_reg<= 1'b0;
+		Bizquierda_reg_ant<=1'b0;
 	end
 	else
 	begin
-		Punt<=Punt_Siguiente;		
+		Punt<=Punt_Siguiente;
+		Bderecha_reg_ant<=Bderecha;			
+		if(~Bderecha_reg_ant && Bderecha)
+		begin
+			Bderecha_reg<=1'b1;
+		end
+		else
+		begin
+			Bderecha_reg<=1'b0;		
+		end
+		Bizquierda_reg_ant<=Bizquierda;			
+		if(~Bizquierda_reg_ant && Bizquierda)
+		begin
+			Bizquierda_reg<=1'b1;
+		end
+		else
+		begin
+			Bizquierda_reg<=1'b0;		
+		end
 	end
 end
 
 always @(*)
 begin
-	Punt_Siguiente=Punt+Bizquierda - Bderecha;
+	Punt_Siguiente=Punt+Bizquierda_reg - Bderecha_reg;
 	if(Bcentro_reg)
 	begin
 		Punt_Siguiente=7'h21;
@@ -350,7 +369,7 @@ begin
 			7'h40:Punt_Siguiente=7'h26;
 		default
 		begin
-			Punt_Siguiente=Punt + Bizquierda - Bderecha;
+			Punt_Siguiente=Punt + Bizquierda_reg - Bderecha_reg;
 		end
 		endcase
 	end
