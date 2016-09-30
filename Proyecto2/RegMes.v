58f450291ad3725be1782fcab73c9b5382084319
Santy241996 <santiago241996@gmail.com>
2016-09-30 08:05:33 -0600

    cambio de parametros

--- Proyecto2/RegMes.v
+++ Proyecto2/RegMes.v
@@ -31,7 +31,7 @@ begin
 	if(UP == 1'b0 &&  DOWN == 1'b1 && Modificando == 1'b1) //Si se decrementa manualmente
 	begin
 	case (Auxiliar)
-			8'h00 : Auxiliar = 8'h12;
+			8'h01 : Auxiliar = 8'h12;
 			8'h10 : Auxiliar = 8'h09;
 			default Auxiliar = Auxiliar - 1'b1;
 	endcase
